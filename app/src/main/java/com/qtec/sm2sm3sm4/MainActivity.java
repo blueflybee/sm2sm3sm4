a85b8cdc53961a0dff6a94ed372fb6a266be1711
shaojun <wusj2017@gmail.com>
2017-07-19 08:54:38 +0800

    Initial commit

---
+++ app/src/main/java/com/qtec/sm2sm3sm4/MainActivity.java
@@ -0,0 +1,41 @@
+package com.qtec.sm2sm3sm4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.blueflybee.library.SM4Utils;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+
+    String plainText = "abcd";
+
+    SM4Utils sm4 = new SM4Utils();
+    sm4.setSecretKey("JeF8U9wHFOMfs2Y8");
+    sm4.setHexString(false);
+
+    System.out.println("ECB模式");
+    String cipherText = sm4.encryptData_ECB(plainText);
+    System.out.println("密文: " + cipherText);
+    System.out.println("");
+
+    plainText = sm4.decryptData_ECB(cipherText);
+    System.out.println("明文: " + plainText);
+    System.out.println("");
+
+
+    System.out.println("CBC模式");
+    sm4.setIv("UISwD9fW6cFh9SNS");
+    cipherText = sm4.encryptData_CBC(plainText);
+    System.out.println("密文: " + cipherText);
+    System.out.println("");
+
+    plainText = sm4.decryptData_CBC(cipherText);
+    System.out.println("明文: " + plainText);
+  }
+}

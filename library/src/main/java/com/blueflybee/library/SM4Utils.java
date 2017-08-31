04165fc0914c15d5eebbd975dc121faeced6c310
shaojun <wusj2017@gmail.com>
2017-08-31 11:15:50 +0800

    add encrypt ecb without base64

--- library/src/main/java/com/blueflybee/library/SM4Utils.java
+++ library/src/main/java/com/blueflybee/library/SM4Utils.java
@@ -47,6 +47,35 @@ public class SM4Utils {
     }
   }
 
+  public byte[] encryptDataNoEncode_ECB(byte[] plainText) {
+    try {
+      SM4_Context ctx = new SM4_Context();
+      ctx.isPadding = true;
+      ctx.mode = SM4.SM4_ENCRYPT;
+
+      byte[] keyBytes;
+      if (hexString) {
+        keyBytes = Util.hexStringToBytes(secretKey);
+      } else {
+        keyBytes = secretKey.getBytes();
+      }
+
+      SM4 sm4 = new SM4();
+      sm4.sm4_setkey_enc(ctx, keyBytes);
+      byte[] encrypted = sm4.sm4_crypt_ecb(ctx, plainText);
+//      String cipherText = new BASE64Encoder().encode(encrypted);
+//      if (cipherText != null && cipherText.trim().length() > 0) {
+//        Pattern p = Pattern.compile("\\s*|\t|\r|\n");
+//        Matcher m = p.matcher(cipherText);
+//        cipherText = m.replaceAll("");
+//      }
+      return encrypted;
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
   public String decryptData_ECB(String cipherText) {
     try {
       SM4_Context ctx = new SM4_Context();
@@ -69,6 +98,31 @@ public class SM4Utils {
       return null;
     }
   }
+
+  public byte[] decryptDataNoDecode_ECB(byte[] cipherText) {
+    try {
+      SM4_Context ctx = new SM4_Context();
+      ctx.isPadding = true;
+      ctx.mode = SM4.SM4_DECRYPT;
+
+      byte[] keyBytes;
+      if (hexString) {
+        keyBytes = Util.hexStringToBytes(secretKey);
+      } else {
+        keyBytes = secretKey.getBytes();
+      }
+
+      SM4 sm4 = new SM4();
+      sm4.sm4_setkey_dec(ctx, keyBytes);
+      byte[] decrypted = sm4.sm4_crypt_ecb(ctx, cipherText);
+      return decrypted;
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+
 
   public String encryptData_CBC(String plainText) {
     try {
@@ -152,6 +206,17 @@ public class SM4Utils {
 
     plainText = sm4.decryptData_CBC(cipherText);
     System.out.println("明文: " + plainText);
+    System.out.println("//////////////////////////////");
+
+
+    System.out.println("ECB模式无编码");
+    byte[] cipherByteText = sm4.encryptDataNoEncode_ECB(plainText.getBytes(UTF_8));
+    System.out.println("密文: " + new String(cipherByteText, UTF_8));
+    System.out.println("");
+
+    cipherByteText = sm4.decryptDataNoDecode_ECB(cipherByteText);
+    System.out.println("明文: " + new String(cipherByteText, UTF_8));
+    System.out.println("");
   }
 
   public String getSecretKey() {

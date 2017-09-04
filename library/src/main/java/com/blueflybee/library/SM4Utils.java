3a8c8a6cbdecd777f8bcc20ce98128a2d1a4d446
shaojun <wusj2017@gmail.com>
2017-09-04 09:37:11 +0800

    add encrypt ecb in byte

--- library/src/main/java/com/blueflybee/library/SM4Utils.java
+++ library/src/main/java/com/blueflybee/library/SM4Utils.java
@@ -48,34 +48,34 @@ public class SM4Utils {
     }
   }
 
-  public byte[] encryptDataNoEncode_ECB(String plainText) {
-    try {
-      SM4_Context ctx = new SM4_Context();
-      ctx.isPadding = true;
-      ctx.mode = SM4.SM4_ENCRYPT;
-
-      byte[] keyBytes;
-      if (hexString) {
-        keyBytes = Util.hexStringToBytes(secretKey);
-      } else {
-        keyBytes = secretKey.getBytes();
-      }
-
-      SM4 sm4 = new SM4();
-      sm4.sm4_setkey_enc(ctx, keyBytes);
-      byte[] encrypted = sm4.sm4_crypt_ecb(ctx, plainText.getBytes(UTF_8));
-//      String cipherText = new BASE64Encoder().encode(encrypted);
-//      if (cipherText != null && cipherText.trim().length() > 0) {
-//        Pattern p = Pattern.compile("\\s*|\t|\r|\n");
-//        Matcher m = p.matcher(cipherText);
-//        cipherText = m.replaceAll("");
+//  public byte[] encryptDataNoEncode_ECB(String plainText) {
+//    try {
+//      SM4_Context ctx = new SM4_Context();
+//      ctx.isPadding = true;
+//      ctx.mode = SM4.SM4_ENCRYPT;
+//
+//      byte[] keyBytes;
+//      if (hexString) {
+//        keyBytes = Util.hexStringToBytes(secretKey);
+//      } else {
+//        keyBytes = secretKey.getBytes();
 //      }
-      return encrypted;
-    } catch (Exception e) {
-      e.printStackTrace();
-      return null;
-    }
-  }
+//
+//      SM4 sm4 = new SM4();
+//      sm4.sm4_setkey_enc(ctx, keyBytes);
+//      byte[] encrypted = sm4.sm4_crypt_ecb(ctx, plainText.getBytes(UTF_8));
+////      String cipherText = new BASE64Encoder().encode(encrypted);
+////      if (cipherText != null && cipherText.trim().length() > 0) {
+////        Pattern p = Pattern.compile("\\s*|\t|\r|\n");
+////        Matcher m = p.matcher(cipherText);
+////        cipherText = m.replaceAll("");
+////      }
+//      return encrypted;
+//    } catch (Exception e) {
+//      e.printStackTrace();
+//      return null;
+//    }
+//  }
 
   public String decryptData_ECB(String cipherText) {
     try {
@@ -100,28 +100,28 @@ public class SM4Utils {
     }
   }
 
-  public byte[] decryptDataNoDecode_ECB(byte[] cipherText) {
-    try {
-      SM4_Context ctx = new SM4_Context();
-      ctx.isPadding = true;
-      ctx.mode = SM4.SM4_DECRYPT;
-
-      byte[] keyBytes;
-      if (hexString) {
-        keyBytes = Util.hexStringToBytes(secretKey);
-      } else {
-        keyBytes = secretKey.getBytes();
-      }
-
-      SM4 sm4 = new SM4();
-      sm4.sm4_setkey_dec(ctx, keyBytes);
-      byte[] decrypted = sm4.sm4_crypt_ecb(ctx, cipherText);
-      return decrypted;
-    } catch (Exception e) {
-      e.printStackTrace();
-      return null;
-    }
-  }
+//  public byte[] decryptDataNoDecode_ECB(byte[] cipherText) {
+//    try {
+//      SM4_Context ctx = new SM4_Context();
+//      ctx.isPadding = true;
+//      ctx.mode = SM4.SM4_DECRYPT;
+//
+//      byte[] keyBytes;
+//      if (hexString) {
+//        keyBytes = Util.hexStringToBytes(secretKey);
+//      } else {
+//        keyBytes = secretKey.getBytes();
+//      }
+//
+//      SM4 sm4 = new SM4();
+//      sm4.sm4_setkey_dec(ctx, keyBytes);
+//      byte[] decrypted = sm4.sm4_crypt_ecb(ctx, cipherText);
+//      return decrypted;
+//    } catch (Exception e) {
+//      e.printStackTrace();
+//      return null;
+//    }
+//  }
 
 
   public String encryptData_CBC(String plainText) {
